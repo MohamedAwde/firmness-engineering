c7c7ba57bf066101efc173582bccd395786461e0
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2022-11-17 15:48:08 +0200

    update team page header

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -6,7 +6,7 @@ export const TeamMember: React.FC<{
   qualification: string;
 }> = ({ name, position, qualification }) => {
   return (
-    <blockquote className="flex flex-col items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-white hover:shadow ease">
+    <blockquote className="flex flex-col items-center justify-between w-full col-span-1 p-6 text-center transition-all duration-200 bg-gray-100 rounded-lg md:flex-row md:text-left hover:bg-orange-500 hover:text-white hover:shadow ease">
       <img
         className="flex-shrink-0 object-cover w-24 h-24 mb-5 bg-gray-300 rounded-full md:mb-0"
         src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1700&amp;q=80"
@@ -28,16 +28,13 @@ export const TeamMember: React.FC<{
 
 const TeamList = () => {
   return (
-    <div className="p-4 sm:p-16">
-      <div className="flex flex-col items-center justify-center w-full h-full max-w-2xl pr-8 mx-auto text-center">
-        <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
-          فريق العمل
-        </h2>
-        <p className="my-6 text-xl font-medium text-gray-500">
-          جيش من اميز الكفائات الهندسية و الادارية كان خلف كل هذة النجاحات و
-          الانجازات مستعد لتحويل احلامك الي واقع
-        </p>
-      </div>
+    <div className="md:p-20 p-4 mt-8">
+      <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
+        اليك فريق العمل{" "}
+      </h2>
+      <h3 className="pt-6 md:pb-16 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
+        نخبة من اميز المهندسين و الادارين جاهزون لخدمتك و خدمة مشاريعك{" "}
+      </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
         <TeamMember
           name="Abd Elmoez Akasha"
